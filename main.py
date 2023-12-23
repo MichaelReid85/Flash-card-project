ddcbff6a565dbab60e01185e1244c648597407e2
MichaelReid85 <Mike1109r@yahoo.com>
2023-12-22 17:34:09 -0800

    finishing it up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pandas
 import random
+import os
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
@@ -9,7 +10,10 @@ to_learn = {}
 try:
     data = pandas.read_csv("data/words_to_learn")
 except FileNotFoundError:
+    ### FOR RUSSIAN ###
     original_data = pandas.read_csv("data/russian_words.csv")
+    ### FOR FRENCH ###
+    # original_data = pandas.read_csv("data/french_words.csv")
     print(original_data)
     to_learn = original_data.to_dict(orient="records")
 else:
@@ -20,10 +24,14 @@ def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
+    ### FOR RUSSIAN ###
     canvas.itemconfig(card_title, text="Russian", fill="black")
     canvas.itemconfig(card_word, text=current_card["Russian"], fill="black")
+    ### FOR FRENCH ###
+    # canvas.itemconfig(card_title, text="French", fill="black")
+    # canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    flip_timer = window.after(3000, func=flip_card())
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
@@ -34,6 +42,10 @@ def flip_card():
 
 def is_known():
     to_learn.remove(current_card)
+    if len(to_learn) == 0:
+        canvas.itemconfig(card_word, text="You Finished Successfully", fill="black")
+        os.remove("data/words_to_learn.csv")
+        window.after(5000, func=quit())
     print(len(to_learn))
     data = pandas.DataFrame(to_learn)
     data.to_csv("data/words_to_learn.csv", index=False)
@@ -46,12 +58,12 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 flip_timer = window.after(3000, func=flip_card)
 
-canvas = Canvas(width= 800, height= 526)
+canvas = Canvas(width= 1000, height= 700)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+card_background = canvas.create_image(500, 350, image=card_front_img)
+card_title = canvas.create_text(500, 200, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(500, 350, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
